9d4545d6c2218207c27870531f85c4f060b8ad1b
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-09-27 08:29:07 +0530

    added signup.jsx file

---
+++ client/src/Signup.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import React from "react-router-dom";
+import axios from 'axios';
+
+function Signup() {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', { name, email, password })
+            .then(result => console.log(result))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-5">
+                <h2>Register</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Name</strong>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Name"
+                            name="name"
+                            className="form-control rounded-0"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                            name="email"
+                            className="form-control rounded-0"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Password</strong>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            outoComplete="off"
+                            name="password"
+                            className="form-control rounded-0"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Register
+                    </button>
+                </form>
+                <p>Already Have an Account</p>
+                <a href="/login" className="btn btn-default w-100 bg-light rounded-0 text-decoration-none">
+                    Login
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
